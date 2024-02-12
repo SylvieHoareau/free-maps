ce6b7976663e18602a52573741a9fdaf0855ff95
SylvieHoareau <hoareausms@gmail.com>
2024-02-12 17:39:26 +0400

    feat: ajout statistiques INSEE en backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // index.js
 import express from 'express';
 import dotenv from 'dotenv';
-import { getSireneData, getPopLegales } from './services/inseeService.js';
+import { getSireneData, getPopLegales, getFilosofiNbMen, getFilosofiZones, getFilosofiDeclarations, getFloresNA17 } from './services/inseeService.js';
 
 // Charger la configuration de dotenv
 dotenv.config()
@@ -88,6 +88,129 @@ app.get('/population', async (req, res) => {
     }
 })
 
+app.get('/filosofi', async (req, res) => {
+    try {
+        // Effectuer la requête de l'API INSEE
+        const filosofiNbData = await getFilosofiNbMen();
+
+        res.render('filosofi', { data: filosofiNbData })
+      
+    } catch (error) {
+        if (error.response) {
+            // La requête a été faite et le serveur a répondu
+            console.error('Erreur de l\'API INSEE', error.response)
+            res.status(error.response.status).send('Erreur de l\'API INSEE')
+            
+        } else if (error.request) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Aucune réponse reçue de l\'API INSEE')
+            res.status(500).send('Erreur lors de la récupération des données')
+        } else if (error) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Erreur lors de la requêt à l\'API INSEE')
+            res.status(401).send('Erreur d\'authentification')
+        } else {
+            // Une erreur s'est produite lors de la configuration de la requête
+            console.error('Erreur lors de la requête à l\'API INSEE', error.message)
+            res.status(500).send('Erreur lors de la requête à l\'API INSEE')
+        }
+        
+    }
+})
+
+app.get('/filosofizones', async (req, res) => {
+    try {
+        // Effectuer la requête de l'API INSEE
+        const filosofiZonesData = await getFilosofiZones();
+
+        res.render('filosofi_zones', { data: filosofiZonesData })
+      
+    } catch (error) {
+        if (error.response) {
+            // La requête a été faite et le serveur a répondu
+            console.error('Erreur de l\'API INSEE', error.response)
+            res.status(error.response.status).send('Erreur de l\'API INSEE')
+            
+        } else if (error.request) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Aucune réponse reçue de l\'API INSEE')
+            res.status(500).send('Erreur lors de la récupération des données')
+        } else if (error) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Erreur lors de la requêt à l\'API INSEE')
+            res.status(401).send('Erreur d\'authentification')
+        } else {
+            // Une erreur s'est produite lors de la configuration de la requête
+            console.error('Erreur lors de la requête à l\'API INSEE', error.message)
+            res.status(500).send('Erreur lors de la requête à l\'API INSEE')
+        }
+        
+    }
+})
+
+
+app.get('/filosofideclarations', async (req, res) => {
+    try {
+        // Effectuer la requête de l'API INSEE
+        const filosofiDeclarationsData = await getFilosofiDeclarations();
+
+        res.render('filosofi_declarations', { data: filosofiDeclarationsData })
+      
+    } catch (error) {
+        if (error.response) {
+            // La requête a été faite et le serveur a répondu
+            console.error('Erreur de l\'API INSEE', error.response)
+            res.status(error.response.status).send('Erreur de l\'API INSEE')
+            
+        } else if (error.request) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Aucune réponse reçue de l\'API INSEE')
+            res.status(500).send('Erreur lors de la récupération des données')
+        } else if (error) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Erreur lors de la requêt à l\'API INSEE')
+            res.status(401).send('Erreur d\'authentification')
+        } else {
+            // Une erreur s'est produite lors de la configuration de la requête
+            console.error('Erreur lors de la requête à l\'API INSEE', error.message)
+            res.status(500).send('Erreur lors de la requête à l\'API INSEE')
+        }
+        
+    }
+})
+
+
+// FLORES
+
+app.get('/floresna17', async (req, res) => {
+    try {
+        // Effectuer la requête de l'API INSEE
+        const floresNa17Data = await getFloresNA17();
+
+        res.render('flores_na17', { data: floresNa17Data })
+      
+    } catch (error) {
+        if (error.response) {
+            // La requête a été faite et le serveur a répondu
+            console.error('Erreur de l\'API INSEE', error.response)
+            res.status(error.response.status).send('Erreur de l\'API INSEE')
+            
+        } else if (error.request) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Aucune réponse reçue de l\'API INSEE')
+            res.status(500).send('Erreur lors de la récupération des données')
+        } else if (error) {
+            // La requête a été faite mais aucune réponse n'a été reçue
+            console.error('Erreur lors de la requêt à l\'API INSEE')
+            res.status(401).send('Erreur d\'authentification')
+        } else {
+            // Une erreur s'est produite lors de la configuration de la requête
+            console.error('Erreur lors de la requête à l\'API INSEE', error.message)
+            res.status(500).send('Erreur lors de la requête à l\'API INSEE')
+        }
+        
+    }
+})
 
 // Démarrer le serveur
 app.listen(port, () => {
--- backend/services/inseeService.js
+++ backend/services/inseeService.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// Sirene
+
 const getSireneData = async () => {
     try {
         const response = await axios.get('https://api.insee.fr/entreprises/sirene/V3/siret?q=codeCommuneEtablissement:97411', {
@@ -13,6 +15,8 @@ const getSireneData = async () => {
         throw new Error(`Error fetching INSEE data: ${error.message}`);
     }
 }
+
+// Populations légales
 
 const getPopLegales = async () => {
     try {
@@ -28,4 +32,72 @@ const getPopLegales = async () => {
     }
 }
 
-export { getSireneData, getPopLegales };
+// Filosofi
+
+const getFilosofiNbMen = async () => {
+    try {
+        const response = await axios.get('https://api.insee.fr/donnees-locales/V0.1/donnees/geo-INDICS_FILO_DISP@GEO2022FILO2020/COM-97411.1', {
+            headers: {
+                'Authorization': `Bearer ${process.env.TOKEN_INSEE}`,
+                'Accept': 'application/json'
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching INSEE data: ${error.message}`);
+    }
+}
+
+const getFilosofiZones = async () => {
+    try {
+        const response = await axios.get('https://api.insee.fr/donnees-locales/V0.1/donnees/geo-INDICS_FILO_DISP_DET@GEO2022FILO2020/COM-97411.1', {
+            headers: {
+                'Authorization': `Bearer ${process.env.TOKEN_INSEE}`,
+                'Accept': 'application/json'
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching INSEE data: ${error.message}`);
+    }
+}
+
+const getFilosofiDeclarations = async () => {
+    try {
+        const response = await axios.get('https://api.insee.fr/donnees-locales/V0.1/donnees/geo-INDICS_FILO_DEC_DET@GEO2022FILO2020/COM-97411.1', {
+            headers: {
+                'Authorization': `Bearer ${process.env.TOKEN_INSEE}`,
+                'Accept': 'application/json'
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching INSEE data: ${error.message}`);
+    }
+}
+
+// Flores
+
+const getFloresNA17 = async () => {
+    try {
+        const response = await axios.get('https://api.insee.fr/donnees-locales/V0.1/donnees/geo-NA17@GEO2022FLORES2020/COM-97411.ALL', {
+            headers: {
+                'Authorization': `Bearer ${process.env.TOKEN_INSEE}`,
+                'Accept': 'application/json'
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching INSEE data: ${error.message}`);
+    }
+}
+
+
+export { 
+    getSireneData,
+    getPopLegales,
+    getFilosofiNbMen,
+    getFilosofiZones,
+    getFilosofiDeclarations,
+    getFloresNA17
+};
